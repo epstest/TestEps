27f23d270c4363baa1f4ba26a4a00d7c56a15aed
dominic <dominic@System-44>
2012-10-30 19:26:45 +0100

    EPS DVT Einführung

---
+++ EPSTEST/src/epstest/EPSTEST.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package epstest;
+import java.io.*;
+/**
+ *
+ * @author dominic
+ */
+public class EPSTEST {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)throws IOException {
+        // TODO code application logic here
+        BufferedReader input = new BufferedReader(new InputStreamReader(
+                System.in));
+        
+        int zahl1,zahl2,ergebnis;
+        
+        zahl1 = Integer.parseInt(input.readLine());
+        zahl2 = Integer.parseInt(input.readLine());
+        ergebnis = zahl1 + zahl2;
+        
+        System.out.printf("%d", ergebnis);
+    }
+}
